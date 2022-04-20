554643a606261b3e41d93198d83d1dcfd3df126a
Jan Thorbecke <jthorbecke@login01.delftblue.tudelft.net>
2022-04-20 11:55:01 +0200

    adding DelftBlue

--- LoadStore/loadStore.c
+++ LoadStore/loadStore.c
@@ -14,14 +14,14 @@ double wallclock_time(void);
 int main ()
 {
 	FILE *fp;
-	int mhz=3200, l3_size=12582912, l2_size=65536, l1_size=32768, l2_line=64;
+	int mhz=3000, l3_size=67108864, l2_size=1048576, l1_size=32768, l2_line=64;
 	int ix, iy, j;
 	size_t power, size, loops;
 	double t0, t1, t_bias, time;
 	double *a, *c, *d, zero=0, *f, *t;
 	char *filename;
 
-	filename="MacM1";
+	filename="DelftBlue";
 	t0  = wallclock_time();
 	t1  = wallclock_time();
 	t_bias = t1-t0;
